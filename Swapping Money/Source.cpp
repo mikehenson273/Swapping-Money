4e73d2848d65bf105532fb6af883f399dbf06e19
MICHAEL HENSON <MICHENSO@uat.edu>
2018-11-15 14:08:56 -0700

    Add project files.

---
+++ Swapping Money/Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+void nowSwapping(int& x, int& y);
+
+int main()
+{
+	int myScore = 150;
+	int yourScore = 1000;
+
+	cout << "Original values\n\n";
+	cout << "My score: " << myScore << "\n";
+	cout << "Your score: " << yourScore << "\n";
+
+	cout << "\nCalling Swap\n\n";
+
+	nowSwapping(myScore, yourScore);
+
+	cout << "swapped scores\n\n";
+	cout << "My score: " << myScore << "\n";
+	cout << "Your score: " << yourScore << "\n";
+
+	system("pause");
+	return 0;
+}
+
+void nowSwapping(int& x, int& y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
